0c618b62a78b8351163419275bd95f0542e3e83f
Edward Delaporte <delaport@illinois.edu>
2024-03-29 14:34:04 -0500

    Add more handy shortcuts

--- example_timecards.py
+++ example_timecards.py
@@ -9,13 +9,7 @@ CIVIL_SERVICE_STANDARD = {
 		'friday': 7.5,
 }
 
-OUT_OF_OFFICE_ALL_WEEK = {
-		'monday': 0, 
-		'tuesday': 0,
-		'wednesday': 0, 
-		'thursday': 0, 
-		'friday': 0,
-}
+STANDARD = CIVIL_SERVICE_STANDARD
 
 MONDAY_OFF = CIVIL_SERVICE_STANDARD.copy()
 MONDAY_OFF['monday'] = 0
@@ -23,11 +17,35 @@ MONDAY_OFF['monday'] = 0
 WED_OFF = CIVIL_SERVICE_STANDARD.copy()
 WED_OFF['wednesday'] = 0
 
+THU_OFF = CIVIL_SERVICE_STANDARD.copy()
+THU_OFF['thursday'] = 0
+
+HALF_DAY_THURS = CIVIL_SERVICE_STANDARD.copy()
+HALF_DAY_THURS['thursday'] = 3.75
+
 FRIDAY_OFF = CIVIL_SERVICE_STANDARD.copy()
 FRIDAY_OFF['friday'] = 0
 
 HALF_DAY_FRIDAY = CIVIL_SERVICE_STANDARD.copy()
 HALF_DAY_FRIDAY['friday'] = 3.75
+
+
+OUT_WED_THURS = CIVIL_SERVICE_STANDARD.copy()
+OUT_WED_THURS['wednesday'] = 0
+OUT_WED_THURS['thursday'] = 0
+
+OUT_THURS_FRI = CIVIL_SERVICE_STANDARD.copy()
+OUT_THURS_FRI['thursday'] = 0
+OUT_THURS_FRI['friday'] = 0
+
+
+OUT_OF_OFFICE_ALL_WEEK = {
+		'monday': 0, 
+		'tuesday': 0,
+		'wednesday': 0, 
+		'thursday': 0, 
+		'friday': 0,
+}
 
 # The keys in this dictionary should all be a Sunday
 # indicating which week to fill in.
